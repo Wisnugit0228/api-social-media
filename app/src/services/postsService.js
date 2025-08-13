d903a0851f56aee0a265b0c5d77c422a1a198805
wisnugit0228 <wisnudwica123@gmail.com>
2025-08-13 13:54:58 +0700

    update S3 bucket using minio

--- app/src/services/postsService.js
+++ app/src/services/postsService.js
@@ -14,13 +14,15 @@ class PostsService {
 
   async uploadImage(image) {
     const s3 = new AWS.S3({
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      endpoint: process.env.MINIO_ENDPOINT,
+      accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+      s3ForcePathStyle: true,
+      signatureVersion: "v4",
     });
     const key = `content/${uuidv4()}-${image.originalname}`;
     const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: process.env.MINIO_BUCKET_NAME,
       Key: key,
       Body: image.buffer,
       ContentType: image.mimetype,
@@ -31,13 +33,15 @@ class PostsService {
 
   async deleteImage(key) {
     const s3 = new AWS.S3({
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      endpoint: process.env.MINIO_ENDPOINT,
+      accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+      s3ForcePathStyle: true,
+      signatureVersion: "v4",
     });
 
     const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: process.env.MINIO_BUCKET_NAME,
       Key: key,
     };
     return s3.deleteObject(params).promise();
--- app/src/services/usersService.js
+++ app/src/services/usersService.js
@@ -12,14 +12,16 @@ class UsersService {
 
   async uploadImage(image) {
     const s3 = new AWS.S3({
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      endpoint: process.env.MINIO_ENDPOINT,
+      accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+      s3ForcePathStyle: true,
+      signatureVersion: "v4",
     });
     const key = `avatar/${uuidv4()}-${image.originalname}`;
 
     const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: process.env.MINIO_BUCKET_NAME,
       Key: key,
       Body: image.buffer,
       ContentType: image.mimetype,
@@ -29,13 +31,15 @@ class UsersService {
 
   async deleteImage(key) {
     const s3 = new AWS.S3({
-      region: process.env.AWS_REGION,
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+      endpoint: process.env.MINIO_ENDPOINT,
+      accessKeyId: process.env.MINIO_ACCESS_KEY_ID,
+      secretAccessKey: process.env.MINIO_SECRET_ACCESS_KEY,
+      s3ForcePathStyle: true,
+      signatureVersion: "v4",
     });
 
     const params = {
-      Bucket: process.env.S3_BUCKET_NAME,
+      Bucket: process.env.MINIO_BUCKET_NAME,
       Key: key,
     };
     return s3.deleteObject(params).promise();
